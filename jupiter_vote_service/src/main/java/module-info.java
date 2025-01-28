ee86afbacc0ae8888d9083cdd9166bd991f5d6bd
jpe7s <jpe7s.salt188@passfwd.com>
2025-01-28 16:23:59 -0600

    Move to Ravina

--- jupiter_vote_service/src/main/java/module-info.java
+++ jupiter_vote_service/src/main/java/module-info.java
@@ -8,8 +8,8 @@ module systems.glam.jupiter_vote_service {
   requires software.sava.solana_web2;
   requires software.sava.solana_programs;
   requires software.sava.anchor_programs;
-  requires software.sava.core_services;
-  requires software.sava.solana_services;
+  requires software.sava.ravina_core;
+  requires software.sava.ravina_solana;
   requires software.sava.kms_core;
 
   uses software.sava.kms.core.signing.SigningServiceFactory;
